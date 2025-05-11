1847351adb12a5afad6b293a61d0f23f656b9a77
Кирилл Катранов <33090898+KirillKatranov@users.noreply.github.com>
2025-05-11 16:37:59 +0300

    Add background music

--- my_ping_pong.py
+++ my_ping_pong.py
@@ -43,9 +43,11 @@ window.fill(back)
 #флаги, отвечающие за состояние игры
 game = True
 finish = False
-clock = time.Clock()
+clock = time.Clock()    
 FPS = 60
-
+mixer.init()
+mixer.music.load("zvuk-skandirovaniya-bolelschikov-25037.ogg")
+mixer.music.play(-1)
 
 #создания мяча и ракетки   
 racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
@@ -64,45 +66,45 @@ speed_y = 3
 
 
 while game:
-   for e in event.get():
-       if e.type == QUIT:
-           game = False
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
   
-   if finish != True:
-       window.fill(back)
-       racket1.update_l()
-       racket2.update_r()
-       ball.rect.x += speed_x
-       ball.rect.y += speed_y
-
-
-       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-           speed_x *= -1
-           speed_y *= 1
-      
-       #если мяч достигает границ экрана, меняем направление его движения
-       if ball.rect.y > win_height-50 or ball.rect.y < 0:
-           speed_y *= -1
-
-
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
-       if ball.rect.x < 0:
-           finish = True
-           window.blit(lose1, (200, 200))
-           game_over = True
-
-
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
-       if ball.rect.x > win_width:
-           finish = True
-           window.blit(lose2, (200, 200))
-           game_over = True
-
-
-       racket1.reset()
-       racket2.reset()
-       ball.reset()
-
-
-   display.update()
-   clock.tick(FPS)
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+ 
+ 
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+       
+        #если мяч достигает границ экрана, меняем направление его движения
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+ 
+ 
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+ 
+ 
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+ 
+ 
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+ 
+ 
+    display.update()
+    clock.tick(FPS)
